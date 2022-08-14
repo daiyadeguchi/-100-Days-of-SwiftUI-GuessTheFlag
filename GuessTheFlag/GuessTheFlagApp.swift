f907418fea7f61fceda09efcd35b9944b6fad838
daiyadeguchi <daiyadeguchi@gmail.com>
2022-08-15 07:31:07 +0900

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Daiya Deguchi on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
